adca68cfdb4e14dd54bd122665f6d7a69711afd1
myat-hsu-mon <myathsumon554@gmail.com>
2021-09-08 16:09:05 +0630

    increase:counter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -7,10 +7,31 @@ import Testimonial from "../components/testimonial"
 import Team from "../components/team"
 
 export default function IndexPage() {
+  const [count, setCount] = useState<number>(1)
+
+  useEffect(() => {
+    const counterIntervalId = setInterval(() => {
+      setCount((count: number) => count + 1)
+    }, 1000)
+
+    setTimeout(() => {
+      clearInterval(counterIntervalId)
+      setCount(0)
+    }, 20000);
+
+    return () => {
+      clearInterval(counterIntervalId)
+    }
+
+  }, [])
+
   return (
     <Layout>
       <Seo title="Home" />
       <Hero />
+      <p className="text-center font-semibold text-gray-400 px-2 py-8">
+        This is Myat Hsu - {count}
+      </p>
       <Testimonial />
       <Team />
     </Layout>
